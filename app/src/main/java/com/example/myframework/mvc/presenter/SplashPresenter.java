6f46f46d250a4900894a70aadd4ea735ab2da5b7
chenhang <chenhang@giec.cn>
2018-11-28 17:52:31 +0800

    seven

--- app/src/main/java/com/example/myframework/mvc/presenter/SplashPresenter.java
+++ app/src/main/java/com/example/myframework/mvc/presenter/SplashPresenter.java
@@ -1,7 +1,15 @@
 package com.example.myframework.mvc.presenter;
 
+import com.example.myframework.AppData;
+import com.example.myframework.dao.AuthUser;
+import com.example.myframework.dao.AuthUserDao;
 import com.example.myframework.dao.DaoSession;
+import com.example.myframework.http.core.HttpObserver;
+import com.example.myframework.http.core.HttpResponse;
 import com.example.myframework.mvc.contract.SplashContract;
+import com.example.myframework.mvc.model.User;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -11,6 +19,8 @@ import javax.inject.Inject;
 
 public class SplashPresenter extends BasePresenter<SplashContract.View> implements SplashContract.Presenter {
 
+    private AuthUser authUser;
+
     @Inject
     public SplashPresenter(DaoSession daoSession) {
         super(daoSession);
@@ -18,8 +28,51 @@ public class SplashPresenter extends BasePresenter<SplashContract.View> implemen
 
     @Override
     public void getUser() {
+        AuthUserDao authUserDao = daoSession.getAuthUserDao();
+        List<AuthUser>users = authUserDao.queryBuilder()
+                .where(AuthUserDao.Properties.Selected.eq(true))
+                .limit(1)
+                .list();
+        AuthUser selectUser = users !=null && users.size()>0 ? users.get(0) : null;
+        if(selectUser == null){
+            List<AuthUser> firstAccount = authUserDao.queryBuilder()
+                    .limit(1)
+                    .list();
+            selectUser = firstAccount !=null && firstAccount.size()>0 ? firstAccount.get(0) : null;
+        }
+        if(selectUser != null && isExpired(selectUser)){
+            authUserDao.delete(selectUser);
+            selectUser = null;
+        }
+        if(selectUser !=null){
+            AppData.INSTANCE.setAuthUser(selectUser);
+            getUserInfo(selectUser.getAccessToken());
+        }else{
+            mView.showLoginPage();
+        }
 
+    }
 
+    private void getUserInfo(final String accessToken) {
+        HttpObserver<User> httpObserver = new HttpObserver<User>() {
+            @Override
+            public void onSuccess(HttpResponse<User> response) {
+                AppData.INSTANCE.setLoggedUser(response.body());
+                if (authUser != null){
+
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        };
+
+    }
+
+    private   boolean isExpired(AuthUser selectUser){
+        return  selectUser.getAuthTime().getTime() + selectUser.getExpireIn()*1000 <System.currentTimeMillis();
     }
 
     @Override
--- app/src/main/java/com/example/myframework/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/myframework/ui/activity/MainActivity.java
@@ -22,62 +22,13 @@ import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG="MainActivity";
-//    TabLayout tabLayout;
-//    ViewPager viewPager;
-//    List<PageModel>pageModels=new ArrayList<>();
-//
-//    {
-//        pageModels.add(new PageModel(R.layout.sample_color,R.string.title_draw_color, R.layout.practice_color));
-//        pageModels.add(new PageModel(R.layout.sample_circle,R.string.title_draw_circle, R.layout.practice_circle));
-//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[]names={"1","2","3"};
-        Observable.from(names)
-                .subscribe(new Action1<String>() {
-                    @Override
-                    public void call(String s) {
-                        Log.d(TAG,s);
-                    }
-                });
-//
-//        viewPager= findViewById(R.id.pager);
-//        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
-//
-//            @Override
-//            public Fragment getItem(int position) {
-//                PageModel pageModel=pageModels.get(position);
-//                return PageFragment.newInstance(pageModel.sampleLayoutRes,pageModel.practiceLayoutRes);
-//            }
-//
-//            @Override
-//            public int getCount() {
-//                return pageModels.size();
-//            }
-//
-//            @Override
-//            public CharSequence getPageTitle(int position) {
-//                return getString(pageModels.get(position).titleRes);
-//            }
-//        });
-//
-//        tabLayout=findViewById(R.id.tabLayout);
-//        tabLayout.setupWithViewPager(viewPager);
     }
 
-//    private class PageModel{
-//        @LayoutRes int sampleLayoutRes;
-//        @StringRes int titleRes;
-//        @LayoutRes int practiceLayoutRes;
-//
-//        PageModel(@LayoutRes int sampleLayoutRes, @StringRes int titleRes, @LayoutRes int practiceLayoutRes) {
-//            this.sampleLayoutRes = sampleLayoutRes;
-//            this.titleRes = titleRes;
-//            this.practiceLayoutRes = practiceLayoutRes;
-//        }
-//    }
 }
